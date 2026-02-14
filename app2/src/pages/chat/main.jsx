606a398b78545b84e3c3e2225e210ece6c214674
Shaun <772542545@qq.com>
2026-02-14 20:53:06 +0800

    。

--- app2/src/pages/chat/main.jsx
+++ app2/src/pages/chat/main.jsx
@@ -8,8 +8,8 @@ export function Chat() {
   const navigate = useNavigate();
   const [title, setTitle] = useState('主页')
   const items2 = [
-    { key: 'friend', display: true, icon: { name: 'users_oline', label: '好友' }, onClick: () => { navigate('/chat/friend/') } },
-    { key: 'news', display: true, icon: { name: 'chat-bubble-bottom-center-text', label: '消息' }, onClick: () => { navigate('/chat/dialog/') } },
+    { key: 'friend', display: true, icon: { name: 'users_oline', label: '好友' }, onClick: () => { navigate('/chat/friend/'); setTitle('好友') } },
+    { key: 'news', display: true, icon: { name: 'chat-bubble-bottom-center-text', label: '消息' }, onClick: () => { navigate('/chat/dialog/'); setTitle('消息') } },
     {
       key: 'users', display: true,
       icon: { name: 'user-group-oline', label: '群聊' },
