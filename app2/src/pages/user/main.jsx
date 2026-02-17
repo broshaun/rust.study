174bc0fff69e07dd0791582db08e8cb6031651f9
Shaun <772542545@qq.com>
2026-02-17 22:20:24 +0800

    .

--- app2/src/pages/user/main.jsx
+++ app2/src/pages/user/main.jsx
@@ -13,9 +13,9 @@ export function User() {
 
   const items = [
     { key: 'home', display: true, icon: { name: 'home', label: '主页' }, onClick: () => navigate('/apps/') },
-
     { key: 'login', display: true, icon: { name: 'user-circle', label: '登陆' }, onClick: () => { navigate('/user/login/'); setTitle('用户登陆'); } },
     { key: 'register', display: true, icon: { name: 'user-plus', label: '注册' }, onClick: () => { navigate('/user/register/'); setTitle('注册账号'); } },
+    { key: 'settings', display: true, icon: { name: 'cog-6-tooth', label: '设置' } },
   ]
 
   const { winSize } = useWinWidth()
