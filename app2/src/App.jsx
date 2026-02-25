7cfe4be2cb425eb2c968a8236bab4b5a8ecbaffb
Shaun <772542545@qq.com>
2026-02-25 10:59:32 +0800

    .

--- app2/src/App.jsx
+++ app2/src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route, Navigate,MemoryRouter } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate, MemoryRouter } from "react-router-dom";
 import Home2 from "pages/home";
 import { RsUser } from "pages/user";
 import { RsChat } from "pages/chat";
@@ -6,21 +6,21 @@ import { useLogin } from "hooks";
 
 const App = () => {
   const { isLogged } = useLogin();
-  
-  return <MemoryRouter>
+
+  return <HashRouter>
     <Routes>
       <Route index
         element={
           isLogged
             ? <Navigate to="/chat/" replace />
             : <Navigate to="/user/login/" replace />
-        }/>
+        } />
       <Route path="apps" element={<Home2 />} />
       {RsUser}
       {RsChat}
 
     </Routes>
-  </MemoryRouter>
+  </HashRouter>
 }
 
 export default App;
