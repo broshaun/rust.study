112e9239fce82980b48de402f150c2c1a4a533d8
Shaun <772542545@qq.com>
2026-03-20 14:31:59 +0800

    修复完成测试版

--- chatly/src/index.jsx
+++ chatly/src/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import '@mantine/core/styles.css';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { MantineProvider } from "@mantine/core";
+import '@mantine/core/styles.css';
 import App from './App';
 
 const container = document.getElementById('root');
