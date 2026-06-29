6eedf51b83035fc5372bbb2cf6e22b890fadbb65
Shaun <772542545@qq.com>
2026-06-29 22:22:34 +0800

    .

--- chatly/src/pages/mobile/chat/myinfo/index.jsx
+++ chatly/src/pages/mobile/chat/myinfo/index.jsx
@@ -5,13 +5,45 @@ import { Logout } from "./logout";
 import { PushDeer } from "./pushdeer";
 import { ClearLogs } from "./clear";
 import { Nickname } from "./nickname";
-import { GlobalModal } from "utils";
+import { GlobalModal, useReady } from "utils";
+import { loginCache } from "cache/loginCache";
+
 
 
 export const MyInfo = () => {
+
+    const [currentUser, setUser] = useState({})
+    useEffect(() => {
+        let isMounted = true;
+        loginCache.fetch().catch(() => { });
+        const unsubscribe = loginCache.subscribe((next) => {
+            if (!isMounted) return;
+            const isObject = next?.data && typeof next.data === 'object';
+            const newData = isObject ? next.data : {};
+            setUser(newData);
+        });
+        return () => {
+            isMounted = false;
+            unsubscribe?.();
+        }
+    }, []);
+
+
+
+    const { ready, data: readyData } = useReady(() => {
+        const uid = userId.get();
+        if (uid && currentUser) {
+            return { uid, currentUser };
+        }
+        return null;
+    }, [currentUser]);
+
+
+
+
     return <div>
         <GlobalModal />
-        <Outlet />
+        <Outlet context={{ ready, readyData }} />
     </div>
 }
 
