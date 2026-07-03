fbc2ea52146fea6894acc97777084a550ef525ef
Shaun <772542545@qq.com>
2026-07-03 12:52:06 +0800

    2

--- chatly/src/cache/friend_list.js
+++ chatly/src/cache/friend_list.js
@@ -7,7 +7,6 @@ import { sessionId } from 'utils/identity';
 const get_friends = async () => {
     const { http } = createHttpClient('/rpc/chat/friend/');
     const results = await http.requestBodyJson("my_friends", {});
-    if (!results) throw new Error("获取失败");
     const { code, data, message } = results;
     if (code !== 200) {
         console.error(message)
